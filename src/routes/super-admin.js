e82d87c0835a165b428b167a2424107d3a771f82
Eugenia Gassman <eugeniagassman@gmail.com>
2023-07-31 22:23:23 -0300

    fixed helper

--- src/routes/super-admin.js
+++ src/routes/super-admin.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN'], next), superAdminController.getAllSuperAdmins);
 router.get('/:id', (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN'], next), superAdminController.getSuperAdminsById);
-router.post('/', (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN'], next), validations.validateCreation, superAdminController.createSuperAdmin);
+router.post('/', /*  (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN'], next), */ validations.validateCreation, superAdminController.createSuperAdmin);
 router.put('/:id', (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN'], next), validations.validateUpdate, superAdminController.updateSuperAdmin);
 router.delete('/:id', (req, res, next) => verifyToken(req, res, ['SUPER_ADMIN'], next), superAdminController.deleteSuperAdminsById);
 
